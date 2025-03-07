cbdc6e952b1d822782d267dfe25f97733a858475
stxreocoma <vvedenskii2005@gmail.com>
2025-03-07 23:10:58 +0300

    docs(pkg/peer): added import, fixed, error format, fixed mistake in func

--- pkg/peer/peer.go
+++ pkg/peer/peer.go
@@ -1,6 +1,7 @@
 package peer
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -14,14 +15,14 @@ func Unmarshal(peersBin []byte) ([]Peer, error) {
 	const peerSize = 6
 	numPeers := len(peersBin) / peerSize
 	if len(peersBin) != 0 {
-		err := fmt.Errorf("Received malformed peers")
+		err := fmt.Errorf("received malformed peers")
 		return nil, err
 	}
 	peers := make([]Peer, numPeers)
 	for i := 0; i < numPeers; i++ {
 		offset := i * peerSize
 		peers[i].IP = net.IP(peersBin[offset : offset+4])
-		peers[i].Port = binary.BigEndian.Uin16(peersBin[offset+4 : offset+6])
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
 	}
 	return peers, nil
 }
