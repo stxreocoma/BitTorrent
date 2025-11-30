d0664a84ebfaf5ac43372a835b7c3534044ce1ec
stxreocoma <vvedenskii2005@gmail.com>
2025-12-01 01:04:37 +0300

    WIP(dialer): added default dialer implementation and panics for http and socks

--- pkg/dialer/dialer.go
+++ pkg/dialer/dialer.go
@@ -23,11 +23,30 @@ type Config struct {
 type DialFunc func(ctx context.Context, address string) (net.Conn, error)
 
 func New(cfg Config) DialFunc {
-	return func(ctx context.Context, address string) (net.Conn, error) {
-		return net.DialTimeout("tcp", address, cfg.Timeout)
+	switch cfg.Type {
+	case HTTPProxy:
+		return BuildHTTPProxyDialer(cfg)
+	case SOCKS5Proxy:
+		return BuildSOCKS5ProxyDialer(cfg)
+	default:
+		return buildDefaultDialer(cfg)
 	}
 }
 
 func NewCustomDialer(dialFunc DialFunc) DialFunc {
 	return dialFunc
+}
+
+func buildDefaultDialer(cfg Config) DialFunc {
+	return func(ctx context.Context, address string) (net.Conn, error) {
+		return net.DialTimeout("tcp", address, cfg.Timeout)
+	}
+}
+
+func BuildHTTPProxyDialer(cfg Config) DialFunc {
+	panic("HTTP proxy support not implemented yet!")
+}
+
+func BuildSOCKS5ProxyDialer(cfg Config) DialFunc {
+	panic("SOCKS proxy support not implemented yet!")
 }
